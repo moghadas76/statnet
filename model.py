831fab487fb0cb39bd2364f81879d1c59e6fea17
moghadas76 <s.m.moghadas2012@gmail.com>
2021-09-04 00:45:50 +0430

    latest

--- model.py
+++ model.py
@@ -555,6 +555,181 @@ class gcn(nn.Module):
 
 
 
+# class gwnet(nn.Module):
+#     def __init__(self, device, num_nodes, dropout=0.3, supports=None, gcn_bool=True, addaptadj=True, aptinit=None, in_dim=2,out_dim=12,residual_channels=32,dilation_channels=32,skip_channels=256,end_channels=512,kernel_size=2,blocks=4,layers=2):
+#         super(gwnet, self).__init__()
+#         self.dropout = dropout
+#         self.blocks = blocks
+#         self.layers = layers
+#         self.gcn_bool = gcn_bool
+#         self.addaptadj = addaptadj
+
+#         self.filter_convs = nn.ModuleList()
+#         self.gate_convs = nn.ModuleList()
+#         self.residual_convs = nn.ModuleList()
+#         self.skip_convs = nn.ModuleList()
+#         self.bn = nn.ModuleList()
+#         self.gconv = nn.ModuleList()
+
+#         self.spatial_attention = SpatialAttention(13,2, num_nodes)
+#         self.temporal_attention = Temporal_Attention_layer(num_nodes,2, 13)
+
+#         self.t_h = nn.Parameter(torch.empty((13)))
+#         nn.init.uniform_(self.t_h, -(1.0 / np.sqrt(2)), (1.0 / np.sqrt(2)))
+#         self.h_x = nn.Parameter(torch.empty(13, 256, 207))
+#         nn.init.uniform_(self.h_x,-(1.0 / np.sqrt(2)), (1.0 / np.sqrt(2)))
+
+#         self.t_h_sp = nn.Parameter(torch.empty((207)))
+#         nn.init.uniform_(self.t_h_sp, -(1.0 / np.sqrt(2)), (1.0 / np.sqrt(2)))
+#         self.h_x_sp = nn.Parameter(torch.empty(207, 256, 207))
+#         nn.init.uniform_(self.h_x_sp,-(1.0 / np.sqrt(2)), (1.0 / np.sqrt(2)))
+
+#         self.start_conv = nn.Conv2d(in_channels=in_dim,
+#                                     out_channels=residual_channels,
+#                                     kernel_size=(1,1))
+#         self.supports = supports
+
+#         receptive_field = 1
+
+#         self.supports_len = 0
+#         if supports is not None:
+#             self.supports_len += len(supports)
+
+#         if gcn_bool and addaptadj:
+#             if aptinit is None:
+#                 if supports is None:
+#                     self.supports = []
+#                 self.nodevec1 = nn.Parameter(torch.randn(num_nodes, 10).to(device), requires_grad=True).to(device)
+#                 self.nodevec2 = nn.Parameter(torch.randn(10, num_nodes).to(device), requires_grad=True).to(device)
+#                 self.supports_len +=1
+#             else:
+#                 if supports is None:
+#                     self.supports = []
+#                 m, p, n = torch.svd(aptinit)
+#                 initemb1 = torch.mm(m[:, :10], torch.diag(p[:10] ** 0.5))
+#                 initemb2 = torch.mm(torch.diag(p[:10] ** 0.5), n[:, :10].t())
+#                 self.nodevec1 = nn.Parameter(initemb1, requires_grad=True).to(device)
+#                 self.nodevec2 = nn.Parameter(initemb2, requires_grad=True).to(device)
+#                 self.supports_len += 1
+
+
+
+
+#         for b in range(blocks):
+#             additional_scope = kernel_size - 1
+#             new_dilation = 1
+#             for i in range(layers):
+#                 # dilated convolutions
+#                 self.filter_convs.append(nn.Conv2d(in_channels=residual_channels,
+#                                                    out_channels=dilation_channels,
+#                                                    kernel_size=(1,kernel_size),dilation=new_dilation))
+
+#                 self.gate_convs.append(nn.Conv1d(in_channels=residual_channels,
+#                                                  out_channels=dilation_channels,
+#                                                  kernel_size=(1, kernel_size), dilation=new_dilation))
+
+#                 # 1x1 convolution for residual connection
+#                 self.residual_convs.append(nn.Conv1d(in_channels=dilation_channels,
+#                                                      out_channels=residual_channels,
+#                                                      kernel_size=(1, 1)))
+
+#                 # 1x1 convolution for skip connection
+#                 self.skip_convs.append(nn.Conv1d(in_channels=dilation_channels,
+#                                                  out_channels=skip_channels,
+#                                                  kernel_size=(1, 1)))
+#                 self.bn.append(nn.BatchNorm2d(residual_channels))
+#                 new_dilation *=2
+#                 receptive_field += additional_scope
+#                 additional_scope *= 2
+#                 if self.gcn_bool:
+#                     self.gconv.append(gcn(dilation_channels,residual_channels,dropout,support_len=self.supports_len))
+
+
+
+#         self.end_conv_1 = nn.Conv2d(in_channels=skip_channels,
+#                                   out_channels=end_channels,
+#                                   kernel_size=(1,1),
+#                                   bias=True)
+
+#         self.end_conv_2 = nn.Conv2d(in_channels=end_channels,
+#                                     out_channels=out_dim,
+#                                     kernel_size=(1,1),
+#                                     bias=True)
+
+#         self.receptive_field = receptive_field
+
+
+
+#     def forward(self, input):
+#         tmp = torch.clone(input)
+#         in_len = input.size(3)
+#         if in_len<self.receptive_field:
+#             x = nn.functional.pad(input,(self.receptive_field-in_len,0,0,0))
+#         else:
+#             x = input
+#         x = self.start_conv(x)
+#         skip = 0
+
+#         # calculate the current adaptive adj matrix once per iteration
+#         new_supports = None
+#         if self.gcn_bool and self.addaptadj and self.supports is not None:
+#             adp = F.softmax(F.relu(torch.mm(self.nodevec1, self.nodevec2)), dim=1)
+#             new_supports = self.supports + [adp]
+
+#         # WaveNet layers
+#         for i in range(self.blocks * self.layers):
+
+#             #            |----------------------------------------|     *residual*
+#             #            |                                        |
+#             #            |    |-- conv -- tanh --|                |
+#             # -> dilate -|----|                  * ----|-- 1x1 -- + -->	*input*
+#             #                 |-- conv -- sigm --|     |
+#             #                                         1x1
+#             #                                          |
+#             # ---------------------------------------> + ------------->	*skip*
+
+#             #(dilation, init_dilation) = self.dilations[i]
+
+#             #residual = dilation_func(x, dilation, init_dilation, i)
+#             residual = x
+#             # dilated convolution
+#             filter = self.filter_convs[i](residual)
+#             filter = torch.tanh(filter)
+#             gate = self.gate_convs[i](residual)
+#             gate = torch.sigmoid(gate)
+#             x = filter * gate
+
+#             # parametrized skip connection
+
+#             s = x
+#             s = self.skip_convs[i](s)
+#             try:
+#                 skip = skip[:, :, :,  -s.size(3):]
+#             except:
+#                 skip = 0
+#             skip = s + skip
+
+
+#             if self.gcn_bool and self.supports is not None:
+#                 if self.addaptadj:
+#                     x = self.gconv[i](x, new_supports)
+#                 else:
+#                     x = self.gconv[i](x,self.supports)
+#             else:
+#                 x = self.residual_convs[i](x)
+
+#             x = x + residual[:, :, :, -x.size(3):]
+
+
+#             x = self.bn[i](x)
+
+#         x = F.relu(skip)
+#         x = x + torch.einsum('nc,cva->nva', (self.spatial_attention(tmp) @ self.t_h_sp , self.h_x_sp)).contiguous().unsqueeze(-1) + \
+#             torch.einsum('nc,cva->nva', (self.temporal_attention(tmp) @ self.t_h, self.h_x)).contiguous().unsqueeze(-1)
+#         x = F.relu(self.end_conv_1(x))
+#         x = self.end_conv_2(x)
+#         return x
+
 class gwnet(nn.Module):
     def __init__(self, device, num_nodes, dropout=0.3, supports=None, gcn_bool=True, addaptadj=True, aptinit=None, in_dim=2,out_dim=12,residual_channels=32,dilation_channels=32,skip_channels=256,end_channels=512,kernel_size=2,blocks=4,layers=2):
         super(gwnet, self).__init__()
@@ -584,6 +759,11 @@ class gwnet(nn.Module):
         self.h_x_sp = nn.Parameter(torch.empty(207, 256, 207))
         nn.init.uniform_(self.h_x_sp,-(1.0 / np.sqrt(2)), (1.0 / np.sqrt(2)))
 
+        self.t_we = nn.Parameter(torch.empty((1)))
+
+        self.s_we = nn.Parameter(torch.empty((1)))
+        nn.init.uniform_(self.t_we,0,1)
+        nn.init.uniform_(self.s_we,0,1)
         self.start_conv = nn.Conv2d(in_channels=in_dim,
                                     out_channels=residual_channels,
                                     kernel_size=(1,1))
@@ -724,8 +904,8 @@ class gwnet(nn.Module):
             x = self.bn[i](x)
 
         x = F.relu(skip)
-        x = x + torch.einsum('nc,cva->nva', (self.spatial_attention(tmp) @ self.t_h_sp , self.h_x_sp)).contiguous().unsqueeze(-1) + \
-            torch.einsum('nc,cva->nva', (self.temporal_attention(tmp) @ self.t_h, self.h_x)).contiguous().unsqueeze(-1)
+        x = x + self.s_we * torch.einsum('nc,cva->nva', (self.spatial_attention(tmp) @ self.t_h_sp , self.h_x_sp)).contiguous().unsqueeze(-1) + \
+            self.t_we * torch.einsum('nc,cva->nva', (self.temporal_attention(tmp) @ self.t_h, self.h_x)).contiguous().unsqueeze(-1)
         x = F.relu(self.end_conv_1(x))
         x = self.end_conv_2(x)
         return x
